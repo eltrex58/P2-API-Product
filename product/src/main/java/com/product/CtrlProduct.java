cdadaafb1a3b8d254606c6eca7f4415984295a75
eltrex58 <fgarciaislas.fgi@ciencias.unam.mx>
2026-09-09 20:06:48 -0600

    CtrlProduct funcionando con los elementos en el código

--- product/src/main/java/com/product/CtrlProduct.java
+++ product/src/main/java/com/product/CtrlProduct.java
@@ -4,13 +4,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List; 
+import java.util.ArrayList;
+
 @RestController 
 @RequestMapping("/category")
 public class CtrlProduct {
 
     @GetMapping
     public String categoryJSON() {
-	    return "Hello World";
+        ServiceCategory service = new ServiceCategory();
+        service.createCategory(new Category("Ropa", "RP", null));
+        service.createCategory(new Category("Calzado", "CLZD", 1));
+        
+	    return service.toString();
     }
 
     
